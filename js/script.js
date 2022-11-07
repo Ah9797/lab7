be153799fb566a2537a3ccd7c787ae9842aacfe7
Ah9797 <89481488+Ah9797@users.noreply.github.com>
2022-11-07 16:47:12 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+function Aclick(){
+  var inputentry = document.getElementById('entryinput').value;
+  var outputresult = document.getElementById('textoutput');
+  outputresult.innerHTML = inputentry;
+  alert("Alexander Hu: " + inputentry);
+  }
+var b = document.getElementById('entrybutton');
+b.addEventListener('click', Aclick);
+}
 window.addEventListener('load', init);
